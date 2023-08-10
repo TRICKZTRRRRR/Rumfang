81b3ff01ddce84fd06e071ce918aece8fd69cc9b
TRICKZTRRRRR <jonas04@outlook.dk>
2023-08-10 13:20:30 +0200

    Add project files.

---
+++ Rumfang/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rumfang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Height");
+            Console.Write("> ");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nWidht");
+            Console.Write("> ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nLenght");
+            Console.Write("> ");
+            double length = double.Parse(Console.ReadLine());
+
+            double volume = height * width * length;
+            
+            Console.WriteLine("\nVolume = " +  volume);
+            Console.ReadLine();
+        }
+    }
+}
